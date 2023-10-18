5cf88123731d03ce7a03cccf81214f31182cf9b4
hefusen <3580476060@qq.com>
2023-10-18 20:17:57 +0800

    新增CommonLineUtil代码

--- 252-commandlineutil/src/main/java/edu/nju/CommandLineUtil.java
+++ 252-commandlineutil/src/main/java/edu/nju/CommandLineUtil.java
@@ -1,13 +1,16 @@
 package edu.nju;
 
-import org.apache.commons.cli.*;
-
-import java.util.Arrays;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class CommandLineUtil {
     private static CommandLine commandLine;
-    private static CommandLineParser parser = new DefaultParser();
-    private static Options options = new Options();
+    private static final CommandLineParser parser = new DefaultParser();
+    private static final Options options = new Options();
     private boolean sideEffect = false;
     public static final String WRONG_MESSAGE = "Invalid input.";
 
@@ -25,12 +28,12 @@ public class CommandLineUtil {
      * step1 add some option rules（you can do it in static{}）
      * step2 parse the input
      * step3 handle options
+     *
      * @param args input of program
      */
-    public void main(String[] args){
-        if(parseInput(args)){
-            handleOptions(args);
-        }
+    public void main(String[] args) {
+        parseInput(args);
+        handleOptions();
     }
 
     /**
@@ -39,51 +42,52 @@ public class CommandLineUtil {
      * but you are recommended to use HelpFormatter to see what will happen
      */
     private static void printHelpMessage() {
-        System.out.print("help\n");
+//        System.out.println("help");
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("cli", options);
     }
 
     /**
      * Parse the input and handle exception
+     *
      * @param args origin args form input
      */
-    public boolean parseInput(String[] args) {
-        if(!Arrays.asList(args).contains("arg0")){
-            System.out.println(WRONG_MESSAGE);
-            return false;
+    public void parseInput(String[] args) {
+        try {
+            commandLine = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println("command line parse failed: " + e.getMessage());
+            System.exit(-1);
         }
-        return true;
     }
 
     /**
      * You can handle options here or create your own func
      */
-    public void handleOptions(String[] args) {
-        try{
-            commandLine = parser.parse(options, args);
-
-            if(commandLine.hasOption("h")){
-                printHelpMessage();
-            }
-            else if(commandLine.hasOption("p")){
-                String content = commandLine.getOptionValue("p");
-                if(content == null){
-                    System.out.println("Missing argument for option: p\n");
-                }
-                else{
-                    System.out.println(content);
-                }
-            }
-            else if(commandLine.hasOption("s")){
-                sideEffect=true;
-            }
-        }catch(ParseException exp){
-            System.out.print("Missing argument for option: p\n");
-            System.exit(-1);
+    public void handleOptions() {
+        if (commandLine.hasOption("h")) {
+            printHelpMessage();
+            return;
         }
 
+        if (commandLine.getArgList().isEmpty()) {
+            System.out.println(WRONG_MESSAGE);
+            return;
+        }
+
+        if (commandLine.hasOption("p")) {
+            String content = commandLine.getOptionValue("p");
+            if (content == null) {
+                System.out.println("Missing argument for option: p");
+            } else {
+                System.out.println(content);
+            }
+        } else if (commandLine.hasOption("s")) {
+            sideEffect = true;
+        }
     }
 
-    public boolean getSideEffectFlag(){
+    public boolean getSideEffectFlag() {
         return sideEffect;
     }
 
